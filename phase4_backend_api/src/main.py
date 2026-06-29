4f50e93fd87df413865f5690c5feeb4283337930
suvadeepd8work-source <suvadeepd8.work@gmail.com>
2026-06-30 01:45:20 +0530

    Remove allow_credentials to allow wildcard CORS
    - Changed allow_credentials from True to False
    - Wildcard origin (*) requires allow_credentials to be False

--- phase4_backend_api/src/main.py
+++ phase4_backend_api/src/main.py
@@ -41,7 +41,7 @@ allowed_origins = os.getenv("ALLOWED_ORIGINS", "*").split(",")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
